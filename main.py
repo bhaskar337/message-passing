d4da00f4f812c00496adf98ef0e738a202c2998d
Vinit <vinit_asher@yahoo.com>
2017-10-26 21:58:49 +0530

    check main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pygame
 import random
+import threading
 from config import *
 from signal import Signal
 from road import Road
 from car import Car
 
 
-class MessagePassing:
 
-	def __init__(self):
+class MessagePassing(threading.Thread):
+	
+
+	def __init__(self,tId,lock):
+		super(MessagePassing,self).__init__()
 		self.road = Road()
 		self.signal1 = Signal(True, 540, 130)
 		self.signal2 = Signal(False, 400, 50)
 		self.car1 = Car(1)
 		self.car2 = Car(2)
+		self.tId = tId
+		self.lock = lock
 
 
 	def render(self):
@@ -36,15 +42,28 @@ class MessagePassing:
 		while True:
 			self.render()
 
-			#random simulation
-			if self.car1.move(self.signal1.on) or self.car2.move(self.signal2.on):
-				if random.getrandbits(1):
-					self.signal1.switch(1)
-					self.signal2.switch(0)
+			# #random simulation
+			# if self.car1.move(self.signal1.on) or self.car2.move(self.signal2.on):
+			# 	if random.getrandbits(1):
+			# 		self.signal1.switch(1)
+			# 		self.signal2.switch(0)
 
-				else:
-					self.signal1.switch(0)
-					self.signal2.switch(1)
+			# 	else:
+			# 		self.signal1.switch(0)
+			# 		self.signal2.switch(1)
+
+			if self.tId == 1:
+				self.lock.acquire()
+				self.car1.move(self.signal1.on)
+				self.signal1.switch(1)
+				self.signal2.switch(0)
+				self.lock.release()
+			else:
+				self.lock.acquire()
+				self.car2.move(self.signal1.on)
+				self.signal1.switch(0)
+				self.signal2.switch(1)
+				self.lock.release()
 
 
 	def close(self):
@@ -53,17 +72,14 @@ class MessagePassing:
 
 
 def main():
-	mp = MessagePassing()
-	mp.run()
+	# mp = MessagePassing()
+	lock = threading.Lock()
+	T1car = MessagePassing(1,lock)
+	T2car = MessagePassing(2,lock)
+	T1car.start()
+	T2car.start()
+	# mp.run()
 
 
 if __name__ == '__main__':
 	main()
-
-
-
-
-
-
-
-
